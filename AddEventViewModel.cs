469b2fe90e5783fbb52d6d601f51f21ea64e02cb
Tanya Buechel <tanyabuechel@gmail.com>
2022-10-19 17:06:59 -0500

    Add project files.

---
+++ AddEventViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(500, ErrorMessage = "Description too long!")]
+        public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+
+        [Required(ErrorMessage ="Location is required.")]
+        public string Location { get; set; }
+
+        [Range(0, 100000, ErrorMessage ="Number of Attendees must be between 0-100,000.")]
+        public int NumberOfAttendees { get; set; }
+    }
+}
